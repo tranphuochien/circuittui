cfe3c930b86709c33c3b80996cfe56d3bfc72de0
hientp <hientp@vng.com.vn>
2018-05-20 14:34:56 +0700

    Refactor code

--- Assets/GenerateChildObject.cs
+++ Assets/GenerateChildObject.cs
@@ -2,6 +2,7 @@
 using System.Timers;
 using UnityEngine;
 using System.Linq;
+using System.Threading;
 
 public class GenerateChildObject : MonoBehaviour {
 
@@ -12,7 +13,7 @@ public class GenerateChildObject : MonoBehaviour {
     private Vector2 BottomRightObject;
     private Vector2 ObjectCenter;
     private Vector2 VObjectScale;
-    public GameObject plane;
+    public GameObject ObjFollowToken;
     public static GameObject listCubes;
     private static bool isCalibrated = false;
     public static Vector3 curPos = new Vector3(0, 0, 0);
@@ -20,13 +21,19 @@ public class GenerateChildObject : MonoBehaviour {
     private float x0;
     private float deltaY;
     private float deltaX;
+    private readonly string CALIBRATE_PLANE = "CalibratePlane";
+    private readonly string MENU = BoardManager.MENU_NAME;
 
     // Use this for initialization
     void Start () {
+        //Find gameobject keep world map 
         listCubes = GameObject.Find("ListCubes");
         listCubes.SetActive(false);
-        plane = GameObject.Find("Menu");
+
+        ObjFollowToken = GameObject.Find(MENU);
         GetCoordinateFromDetection();
+
+        CalibratePlane = GameObject.Find(CALIBRATE_PLANE);
     }
 	
 	// Update is called once per frame
@@ -38,15 +45,14 @@ public class GenerateChildObject : MonoBehaviour {
             {
                 listCubes.SetActive(true);
                 RayCastController.beginRayCasting = true;
-                if (GameObject.Find("Phone") != null)
+                if (GameObject.Find(CALIBRATE_PLANE) != null)
                 {
-                    GameObject.Find("Phone").SetActive(false);
-                    GameObject.Find("Menu").SetActive(true);
+                    GameObject.Find(CALIBRATE_PLANE).SetActive(false);
+                    GameObject.Find(MENU).SetActive(true);
                 }
-                //ObjectCenter = CalculateCenter(TopLeftObject, BottomRightObject);
                 ObjectCenter = new Vector2(SocketClient.xPos, SocketClient.yPos);
                 CalculateScale(TopLeftScreen, BottomRightScreen, TopLeftObject, BottomRightObject);
-                if (plane == null)
+                if (ObjFollowToken == null)
                 {
                     GenerateChild();
                 }
@@ -56,49 +62,52 @@ public class GenerateChildObject : MonoBehaviour {
                 }
             } else
             {
-                if (GameObject.Find("Menu"))
+                if (GameObject.Find(MENU))
                 {
-                    GameObject.Find("Menu").SetActive(false);
-                    GameObject.Find("Phone").SetActive(true);
+                    GameObject.Find(MENU).SetActive(false);
+                    GameObject.Find(CALIBRATE_PLANE).SetActive(true);
                 }
-                phone = GameObject.Find("Phone");
-                //CalibrateCoordination();
+                
                 Calibrate();
             }
         }
-        
     }
 
-    int maxData_temp = 90;
-    int maxData = 15;
+    //At a position, get maxDataCollect entries from detector system
+    int maxDataCollect = 90;
+    //Number of positions at X(Y) axis need collect data to calibrate
     int maxNumX = 9, maxNumY = 6;
-    GameObject phone;
+    //Must equal: maxNumX + maxNumY
+    int maxData = 15; 
+    GameObject CalibratePlane;
     private List<Vector2> listData_temp = new List<Vector2>();
-    private List<Vector2> listDataY_temp = new List<Vector2>();
     private List<Vector2> listData = new List<Vector2>();
-    private List<Vector2> listDataY = new List<Vector2>();
-    private int a = 0;
+
+    private int count = 0;
    
     private void Calibrate()
     {
         if (listData.Count < maxData)
         {
-            if (listData_temp.Count < maxData_temp)
+            if (listData_temp.Count < maxDataCollect)
             {
-                phone.transform.localPosition = a < maxNumX ? new Vector3(a, 0.1f, 0) : new Vector3(0, 0.1f, -((a - maxNumX) % maxNumY));
-                for (int temp = 0; temp < 500; temp++) ;
+                //Move CalibratePlane to collect raw data position from detetor system
+                CalibratePlane.transform.localPosition = count < maxNumX ? new Vector3(count, 0.1f, 0) : new Vector3(0, 0.1f, -((count - maxNumX) % maxNumY));
+                //Delay for Detect system track object again after movement
+                DelayUIThread(50);
+                //Mapping raw coordinate to ScreenCenter coordinate
                 listData_temp.Add(new Vector2(SocketClient.xPos - ScreenCenter.x, SocketClient.yPos - ScreenCenter.y));
             }
             else
             {
                 listData.Add(new Vector2(listData_temp.Average(point => point.x), listData_temp.Average(point => point.y)));
                 listData_temp.Clear();
-                a++;
+                count++;
             }
         }
         if (listData.Count == maxData)
         {
-            phone.SetActive(false);
+            CalibratePlane.SetActive(false);
             listData[maxNumX] = listData[0];
             x0 = listData[0].x;
             y0 = listData[0].y;
@@ -111,10 +120,16 @@ public class GenerateChildObject : MonoBehaviour {
                     listY.Add(listData[i + maxNumX].y - listData[i + maxNumX - 1].y);
                 } 
             }
-            deltaX = listX.Average(); //(listData[3].x - listData[0].x) / 3.0f;
-            deltaY = listY.Average(); //(listData[7].y - listData[4].y) / 3.0f;
+            deltaX = listX.Average();
+            deltaY = listY.Average();
             isCalibrated = true;
+            GameObject.Find("Plane").GetComponent<MeshRenderer>().material = Resources.Load("wood", typeof(Material)) as Material;
         }
+    }
+
+    private void DelayUIThread(int milliseconds)
+    {
+        Thread.Sleep(milliseconds);
     }
 
     private void MovePlane()
@@ -122,19 +137,19 @@ public class GenerateChildObject : MonoBehaviour {
         ObjectCenter -= ScreenCenter;
         Vector3 newPos = new Vector3((ObjectCenter.x - x0) / deltaX, 0.1f, (y0 - ObjectCenter.y) / deltaY);
         curPos = newPos;
-        plane.transform.localPosition = Vector3.Lerp(plane.transform.localPosition, newPos, Time.deltaTime * 5.0f);
+        ObjFollowToken.transform.localPosition = Vector3.Lerp(ObjFollowToken.transform.localPosition, newPos, Time.deltaTime * 5.0f);
     }
 
     private void GenerateChild()
     {
-        plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        plane.transform.name = "ABCD";
-        plane.transform.SetParent(this.gameObject.transform);
+        ObjFollowToken = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        ObjFollowToken.transform.name = "ABCD";
+        ObjFollowToken.transform.SetParent(this.gameObject.transform);
         ObjectCenter -= ScreenCenter;
-        plane.transform.localRotation = Quaternion.Euler(0, 0, 0);
-        plane.transform.localPosition = new Vector3((ObjectCenter.x - x0) / deltaX, 0.1f, (y0 - ObjectCenter.y) / deltaY);
-        plane.transform.localScale = new Vector3(VObjectScale.x, 1, VObjectScale.y);
-        plane.GetComponent<MeshRenderer>().material = Resources.Load("white", typeof(Material)) as Material;
+        ObjFollowToken.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        ObjFollowToken.transform.localPosition = new Vector3((ObjectCenter.x - x0) / deltaX, 0.1f, (y0 - ObjectCenter.y) / deltaY);
+        ObjFollowToken.transform.localScale = new Vector3(VObjectScale.x, 1, VObjectScale.y);
+        ObjFollowToken.GetComponent<MeshRenderer>().material = Resources.Load("white_transparent", typeof(Material)) as Material;
     }
 
     private void CalculateScale(Vector2 topLeftScreen, Vector2 bottomRightScreen, Vector2 topLeftObject, Vector2 bottomRightObject)
--- Assets/RayCastController.cs
+++ Assets/RayCastController.cs
@@ -10,9 +10,12 @@ public class RayCastController : MonoBehaviour {
     public Vector3 directionZ = new Vector3(0, 0, -1);
   
     public static bool beginRayCasting = false;
+    private GameObject screenObject;
+    private readonly string TAG = "cube";
 
     // Use this for initialization
     void Start () {
+        screenObject = GameObject.Find("Screen");
 	}
   
     // Update is called once per frame
@@ -24,10 +27,10 @@ public class RayCastController : MonoBehaviour {
         ResetColor();
         RaycastHit hit;
         
-        Vector3 test = new Vector3(GenerateChildObject.curPos.x + 13.57f, GenerateChildObject.curPos.z + 9.94f, z);
+        Vector3 test = new Vector3(GenerateChildObject.curPos.x + screenObject.transform.position.x, GenerateChildObject.curPos.z + screenObject.transform.position.y, z);
         if (Physics.Raycast(test, directionZ, out hit, 100))
         {
-            if (hit.collider.gameObject.tag == "cube")
+            if (hit.collider.gameObject.tag == TAG)
             {
                 hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
             }
